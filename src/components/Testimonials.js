9512619e8263065eb88842a74686fb835d28064a
Hannah Stahl <hls62@cornell.edu>
2021-06-06 16:25:30 -0500

    Styled testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,8 +7,8 @@ const Testimonials = () => (
     <div className="content-container testimonials">
       {content.testimonials.map(({ quote, source }) => (
         <div key={source} className="testimonial">
-          <p>{`"${quote}"`}</p>
-          <p><b>{`- ${source}`}</b></p>
+          <p>{`“${quote}”`}</p>
+          <p className="source">{`- ${source}`}</p>
         </div>
       ))}
     </div>
