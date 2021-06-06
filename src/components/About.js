52efa5058a0fc95ad8a9cbcb9143ef08952d4a2a
Hannah Stahl <hls62@cornell.edu>
2021-06-06 12:09:59 -0500

    Added image placeholder to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import content from '../content.json';
+import config from '../config';
 
 const About = () => (
   <div>
     <h1>About</h1>
-    <div className="content-container">
+    <div className="content-container about">
+      <div className="img-placeholder"><p>INSERT HEADSHOT HERE</p></div>
+      {/* <img src={`${config.publicCloudfrontURL}/stahl-speech-bio.jpg`} alt="Ally Stahl" /> */}
       {content.bio.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
     </div>
   </div>
