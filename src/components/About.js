a83b61c0be8d74827f56eaa9e5e4c1331ad08adb
Hannah Stahl <hls62@cornell.edu>
2021-06-19 12:03:31 -0500

    Added About page photo

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import content from '../content.json';
-// import config from '../config';
+import config from '../config';
 
 const About = () => (
   <div>
     <div className="content-container about">
-      <div className="img-placeholder"><p>INSERT HEADSHOT HERE</p></div>
-      {/* <img src={`${config.publicCloudfrontURL}/dalefeuerjewelry-bio.jpg`} alt="Ally Stahl" /> */}
+      <img src={`${config.publicCloudfrontURL}/stahl-speech-about.jpg`} alt="Ally Stahl" />
       <div>
         <h2>About Ally</h2>
         {content.bio.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
