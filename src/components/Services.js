79b2e19a5d32848570e6cceab424dd1850a34803
Hannah Stahl <hls62@cornell.edu>
2021-06-24 21:26:30 -0500

    Redesigned Services pge

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,19 +8,19 @@ const Services = () => {
   return content ? (
     <div>
       <div className="content-container services">
-        <div key={content.Services.section1.title} className="service">
+        <div className="who-we-serve">
           <div className="service-description">
             <h2>{content.Services.section1.title}</h2>
             <PortableText text={content.Services.section1.descriptionRaw} />
           </div>
-          <img src={`${config.publicCloudfrontURL}/stahl-speech-who-we-serve-2.jpeg`} alt={content.Services.section1.title} />
+          <img
+            src={`${config.publicCloudfrontURL}/stahl-speech-services.jpeg`}
+            alt={content.Services.section1.title}
+          />
         </div>
-        <div key={content.Services.section1.title} className="service reverse">
-          <img src={`${config.publicCloudfrontURL}/stahl-speech-services-provided-2.jpeg`} alt={content.Services.section2.title} />
-          <div className="service-description">
-            <h2>{content.Services.section2.title}</h2>
-            <PortableText text={content.Services.section2.descriptionRaw} />
-          </div>
+        <div>
+          <h2>{content.Services.section2.title}</h2>
+          <PortableText text={content.Services.section2.descriptionRaw} />
         </div>
       </div>
     </div>
